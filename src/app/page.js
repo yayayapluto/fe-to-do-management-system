c2cf13f70a1288a8305e2fa7e56fd46a2933c6c9
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-01-18 15:41:58 +0700

    remove footer

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -96,7 +96,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
